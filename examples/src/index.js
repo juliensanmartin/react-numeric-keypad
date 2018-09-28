3951c5140fa87d1b2a839191b0badedaa3907394
Julien Sanmartin <sanmartinjulien@gmail.com>
2018-09-28 10:45:01 -0700

    bootstrap project

---
+++ examples/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Keypad from '../../src';
+
+const App = () => <Keypad />;
+
+render(<App />, document.getElementById('root'));
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './styles.css';
+
+const Keypad = () => (
+    <h1>Hello from My Component</h1>
+);
+export default Keypad;
